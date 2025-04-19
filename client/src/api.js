594da18c2ff0ff1421031a042fccfcf584ecdcb5
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2025-04-19 17:21:15 +1000

    Update env variable for API base URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const getLivePrice = async (coinId, currency = 'usd') => {
   const res = await fetch(`${BASE_URL}/price/${coinId}?currency=${currency}`);
   const data = await res.json();
   return data[currency];
 };
+
